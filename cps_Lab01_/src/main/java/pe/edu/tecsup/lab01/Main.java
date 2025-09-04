b6cecb0bfd2fb5a850bce5f6fe61f2b7b81255bf
Luladusilva <gabriela1.8.wib@gmail.com>
2025-09-03 22:59:31 -0500

    crear clase controller

---
+++ cps_Lab01_/src/main/java/pe/edu/tecsup/lab01/Main.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab01;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public String reverseString(String src) {
+        return new StringBuilder(src).reverse().toString();
+    }
+}
---
+++ cps_Lab01_/src/test/java/pe/edu/tecsup/lab01/MainTest.java
@@ -0,0 +1,15 @@
+package pe.edu.tecsup.lab01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void reverseString() {
+        Main app = new Main();
+        final String EXPECTED = "!aloH";
+        String actual = app.reverseString("Hola!");
+        // validate
+        assertEquals(EXPECTED, actual);
+    }
+}
